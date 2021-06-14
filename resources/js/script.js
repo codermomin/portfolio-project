a006ee45146b4477c3258e3b000755921d57bec3
codermomin <85857427+codermomin@users.noreply.github.com>
2021-06-14 13:19:27 +0600

    Added My First Work

---
+++ resources/js/script.js
@@ -0,0 +1,2 @@
+var mixer=mixitup('.grid-portfolio-container'); function openMenu(){document.getElementById('navbar').style.height="100%"}function closeMenu(){document.getElementById('navbar').style.height="0%"}$(function(){$('.menu-item a, .scroll-down a, .smooth').on('click', function(){$('html, body').animate({scrollTop: $($.attr(this, 'href')).offset().top}, 1000); return false});});
+ 
